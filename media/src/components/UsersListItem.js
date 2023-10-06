ca6fe91901d9c9e4d45ce131f14ed156aaa1609b
Hakan Baştürk <forxynet@gmail.com>
2023-10-06 17:37:14 +0300

    Created and Endpoint

--- media/src/components/UsersListItem.js
+++ media/src/components/UsersListItem.js
@@ -2,6 +2,8 @@ import { FaRegTrashCan } from "react-icons/fa6";
 import Button from './Button';
 import { removeUser } from '../store';
 import { useThunk } from '../hooks/use-thunk';
+import ExpandablePnael  from "./ExpandablePanel";
+import AlbumsList from './AlbumsList';
 
 function UsersListItem({ user }) {
   const [doRemoveUser, isLoading, error] = useThunk(removeUser);
@@ -10,18 +12,18 @@ function UsersListItem({ user }) {
     doRemoveUser(user);
   };
 
-  return (
-    <div className="mb-2 border rounded">
-      <div className="flex p-2 justify-between items-center cursor-pointer">
-        <div className="flex flex-row items-center justify-between">
-          <Button className="mr-3" loading={isLoading} onClick={handleClick}>
+  const header = <>
+    <Button className="mr-3" loading={isLoading} onClick={handleClick}>
             <FaRegTrashCan />
           </Button>
           {error && <div>Error deleting user.</div>}
           {user.name}
-        </div>
-      </div>
-    </div>
+  </>;
+
+  return (  
+  <ExpandablePnael header={header}>
+    <AlbumsList user={user} />
+  </ExpandablePnael>   
   );
 }
 

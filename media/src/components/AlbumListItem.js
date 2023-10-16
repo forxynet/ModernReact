e7dfcd1271d6ae4010563b9aec13e4ea6bd738b4
Hakan Baştürk <forxynet@gmail.com>
2023-10-16 17:28:07 +0300

    added photos api and list

---
+++ media/src/components/AlbumListItem.js
@@ -0,0 +1,34 @@
+import { FaRegTrashCan } from "react-icons/fa6";
+import { useRemoveAlbumMutation } from '../store';
+import Button from './Button';
+import ExpandablePanel from './ExpandablePanel';
+import PhotosList from './PhotosList';
+
+function AlbumsListItem({ album }) {
+  const [removeAlbum, results] = useRemoveAlbumMutation();
+
+  const handleRemoveAlbum = () => {
+    removeAlbum(album);
+  };
+
+  const header = (
+    <>
+      <Button
+        className="mr-2"
+        loading={results.isLoading}
+        onClick={handleRemoveAlbum}
+      >
+        <FaRegTrashCan />
+      </Button>
+      {album.title}
+    </>
+  );
+
+  return (
+    <ExpandablePanel key={album.id} header={header}>
+      <PhotosList album={album} />
+    </ExpandablePanel>
+  );
+}
+
+export default AlbumsListItem;
--- media/src/components/AlbumsList.js
+++ media/src/components/AlbumsList.js
@@ -1,41 +1,37 @@
-import { useFetchAlbumsQuery, useAddAlbumMutation } from "../store";
+import { useFetchAlbumsQuery, useAddAlbumMutation } from '../store';
 import Skeleton from './Skeleton';
 import ExpandablePanel from './ExpandablePanel';
 import Button from './Button';
+import AlbumsListItem from './AlbumListItem';
 
 function AlbumsList({ user }) {
-  const { data, error, isLoading } = useFetchAlbumsQuery(user);
+  const { data, error, isFetching } = useFetchAlbumsQuery(user);
   const [addAlbum, results] = useAddAlbumMutation();
 
-  const handleAddAblbum = () => {
+  const handleAddAlbum = () => {
     addAlbum(user);
   };
 
   let content;
-  if(isLoading){
-    content = <Skeleton times={3} />;
-  }else if(error){
-    content = <div>Error loading albums.</div>
+  if (isFetching) {
+    content = <Skeleton className="h-10 w-full" times={3} />;
+  } else if (error) {
+    content = <div>Error loading albums.</div>;
   } else {
-    content = data.map(album => {
-        const header = <div>{album.tite}</div>
-        return <ExpandablePanel key={album.id} header={header}>
-            List of fotos in the album
-        </ExpandablePanel>
+    content = data.map((album) => {
+      return <AlbumsListItem key={album.id} album={album} />
     });
   }
 
-  return <div>
+  return (
     <div>
-        Albums for {user.name}
-        <Button onClick={handleAddAblbum}>
-            + Ad Album
-        </Button>
+      <div className='m-2 flex flex-row items-center justify-between'>
+        <h3 className='text-lg font-bold'>Albums for {user.name}</h3>
+        <Button loading={results.isLoading} onClick={handleAddAlbum}>+ Add Album</Button>
+      </div>
+      <div>{content}</div>
     </div>
-    <div>
-        {content}
-    </div>
-  </div> 
+  );
 }
 
 export default AlbumsList;

feff59a2223d563c227af18795ae5e26143c590c
hakan.basturknewinn.com <forxynet@gmail.com>
2024-08-19 20:16:35 +0300

    location search app complated

--- media/src/components/UsersList.js
+++ media/src/components/UsersList.js
@@ -7,8 +7,7 @@ import { useThunk } from '../hooks/use-thunk';
 import UsersListItem from './UsersListItem';
 
 function UsersList() {
-  const [doFetchUsers, isLoadingUsers, loadingUsersError] =
-    useThunk(fetchUsers);
+  const [doFetchUsers, isLoadingUsers, loadingUsersError] = useThunk(fetchUsers);
   const [doCreateUser, isCreatingUser, creatingUserError] = useThunk(addUser);
   const { data } = useSelector((state) => {
     return state.users;
